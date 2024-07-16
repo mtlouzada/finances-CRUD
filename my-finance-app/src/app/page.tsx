24adceebe7ff2dfc845c092b6f2cfbaaa9f806b0
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-07-16 00:11:44 -0300

    feat: create other app

---
+++ my-finance-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Finance App</h1>
+      </div>
+    </main>
+  );
+}
