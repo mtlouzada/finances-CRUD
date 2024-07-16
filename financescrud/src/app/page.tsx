2927212a60de25aa5a6682cadd06e2fc9e514124
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-07-15 22:13:39 -0300

    feat: create next application for make front-end part

---
+++ financescrud/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+       <h1>Finances CRUD</h1>
+       
+      </div>
+    </main>
+  );
+}
