eab7cbd78d2f0e197814add6af7b0d487a274c9b
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-07-18 12:15:12 -0300

    feat: start coding skeleton and style from table

---
+++ my-finance-app/src/app/components/Table/Table.tsx
@@ -0,0 +1,20 @@
+import './table.scss'
+
+export function Table(){
+    return(
+        <div className='container-tabela'>
+            <form action="" method='get'>
+                <label htmlFor="fname">Nome: </label>
+                <input type="text" id='fname' name='fname'/>
+
+                <label htmlFor="wage">Salário: </label>
+                <input type="number" id='wage' name='wage'/>
+
+                <label htmlFor="exits">Saídas</label>
+                <input type="number" id='exits' name='exits'/>
+
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
+    );
+}
