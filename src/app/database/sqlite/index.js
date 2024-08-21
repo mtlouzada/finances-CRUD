e95a4f0d9da582aeb9f8f9ae5b69b10c8ea20925
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-08-21 16:46:22 -0300

    index db-connection

--- src/app/database/sqlite/index.js
+++ src/app/database/sqlite/index.js
@@ -1,8 +1,14 @@
 const sqlite3 = require("sqlite3");
 const sqlite = require("sqilte");
+const path = require("path");
 
 async function sqliteConnnection() {
     const database = await sqlite.open({
-        
-    })
+    filename: path.resolve(__dirname, "..", "database.db"),
+    driver: sqlite3.Database
+    });
+
+    return database;
 }
+
+module.exports = sqliteConnnection;
--- src/app/service/dbConnection.js
+++ src/app/service/dbConnection.js
@@ -1,11 +1,1 @@
-const mysql = require('mysql');
-
-    module.exports = function(app) {
-        return mysql.createConnection({
-            host : 'localhost',
-            user : 'root',
-            password : '',
-            database : 'portal_noticias',
-        });
-
-    };
+teste
