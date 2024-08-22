8f4bcc4ceced2ee8244d95367ade72f3e351cbee
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-08-22 16:50:31 -0300

    try fix build error

--- src/app/components/Table/Table.tsx
+++ src/app/components/Table/Table.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import React from 'react';
 import { useState } from 'react';
 import './Table.scss';
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use cliente";
+
 import { Header } from "./components/Header/Header";
 import { Table } from "./components/Table/Table";
 import "./page.module.scss";
