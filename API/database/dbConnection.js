2fbe0cbe6d47b72a04cd026cd6903c308c9e0f39
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-09-20 23:20:52 -0300

    singleton desing pattern

--- API/database/dbConnection.js
+++ API/database/dbConnection.js
@@ -1,16 +1,24 @@
 const sqlite3 = require("sqlite3");
 const path = require("path");
 
+let dbInstance = null;
+
 function createDbConnection() {
-    var filepath = "./database/finance.db";
-    filepath = path.resolve(__dirname, "..", filepath);
+  if (dbInstance) {
+    return dbInstance;
+  }
+
+  var filepath = "./database/finance.db";
+  filepath = path.resolve(__dirname, "..", filepath);
 
   const db = new sqlite3.Database(filepath, (error) => {
     if (error) {
       return console.error(error.message);
     }
+    console.log("Connection with SQLite has been established");
   });
-  console.log("Connection with SQLite has been established");
+  
+  dbInstance = db
   return db;
 }
 
