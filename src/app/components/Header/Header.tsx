6708fc17e7909d5b5264244c52f6c0a86b31d45d
mtlouzada <matheuslouzadaa@gmail.com>
2024-10-12 23:38:56 -0300

    teste tailwind style

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export function Header() {
     return( 
-        <div className="bg-black ">
+        <div className="bg-black text-sky-400">
             <h1>Finances CRUD</h1>
         </div>
     );
